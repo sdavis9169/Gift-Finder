7ef345f2bf70f78457af880e6d9572a47140ecb6
Sdavis <stevendavis0622@gmail.com>
2020-07-13 20:01:40 -0600

    Basic navbar, router set up, Home and About components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: 'Gift Finder',
+  icon: 'fa fa-gift'
+};
+
+export default Navbar;
